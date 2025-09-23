83e7d342268a8cc08a89e9f8e9a5fea5ae6822ae
TheCreatorNode <basseyu01@gmail.com>
2025-09-23 12:48:22 +0100

    Add basic test setup and passed test function

--- src/DEXLite.sol
+++ src/DEXLite.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract DEXLite is Ownable{
-    
-    IERC20  public tokenA;
-    IERC20  public tokenB;
+contract DEXLite is Ownable {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
 
-    struct Liquidator{
+    struct Liquidator {
         bool permitted;
         uint256 tokenA;
         uint256 tokenB;
@@ -20,37 +19,32 @@ contract DEXLite is Ownable{
     uint256 public reservesA;
     uint256 public reservesB;
 
-    
-
     //custom Error
-    error invalidAdddress();
+    error invalidAddress();
     error onlyLiquidatorPermitted();
     error invalidLiquidity();
 
     //modifier
     modifier onlyLiquidator() {
-        if(!liquidator[msg.sender].permitted) revert onlyLiquidatorPermitted();
+        if (!liquidator[msg.sender].permitted) revert onlyLiquidatorPermitted();
         _;
     }
 
-    constructor(IERC20 _tokenA, IERC20 _tokenB)
-    Ownable(){
-        if(_tokenA == address(0) || _tokenB == address(0)) revert invalidAddress();
+    constructor(IERC20 _tokenA, IERC20 _tokenB, address _initialOwner) Ownable(_initialOwner) {
+        if (address(_tokenA) == address(0) || address(_tokenB) == address(0)) revert invalidAddress();
         tokenA = _tokenA;
         tokenB = _tokenB;
     }
 
-    function addLiquidity(uint256 amountA, uint256 amountB){
-        if(amountA == 0 && amountB == 0) revert invalidLiquidity();
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        if (amountA == 0 || amountB == 0) revert invalidLiquidity();
 
-        require(tranferFrom(msg.sender, address(this), amountA), "Tranfer Failed");
-        require(transferFrom(msg.sender, address(this), amountB), "Transfer Failed");
+        require(tokenA.transferFrom(msg.sender, address(this), amountA), "Tranfer Failed");
+        require(tokenB.transferFrom(msg.sender, address(this), amountB), "Transfer Failed");
 
         reservesA += amountA;
         reservesB += amountB;
 
         liquidator[msg.sender] = Liquidator(true, amountA, amountB);
     }
-
-    
 }
--- test/DEXLite.t.sol
+++ test/DEXLite.t.sol
@@ -3,7 +3,60 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DEXLite} from "../src/DEXLite.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+contract MockERC20 is ERC20{
+    constructor(string memory name, string memory symbol) 
+    ERC20(name, symbol){}
 
-contract CounterTest is Test {
-  
+    function mint(address to, uint256 amount) public{
+        _mint(to, amount);
+    }
+}
+
+contract DEXLiteTest is Test {
+    DEXLite public DEX;
+    MockERC20 public mockTokenA;
+    MockERC20 public mockTokenB;
+
+    address public owner = makeAddr("owner");
+    address public liquidator = makeAddr("liquidator");
+    address public user = makeAddr("user");
+    
+    function setUp() public {
+        mockTokenA = new MockERC20("tokenA", "TA");
+        mockTokenB = new MockERC20("tokenB", "TB");
+
+        DEX = new DEXLite(IERC20(address(mockTokenA)), IERC20(address(mockTokenB)), address(this));
+
+        //mint token to liquidator
+        mockTokenA.mint(liquidator, 1000 * 1e18);
+        mockTokenB.mint(liquidator, 500 * 1e18);
+    }
+
+    function testAddLiquidity() public{
+        vm.prank(liquidator);
+        mockTokenA.approve(address(DEX), 100 * 1e18);
+        vm.prank(liquidator);
+         mockTokenB.approve(address(DEX), 50 * 1e18);
+        vm.prank(liquidator);
+        DEX.addLiquidity(100 * 1e18, 50 * 1e18);
+        
+        assertEq(DEX.reservesA(), 100 * 1e18);
+        assertEq(DEX.reservesB(), 50 * 1e18);
+        (bool permitted, uint256 tokenA, uint256 tokenB) = DEX.liquidator(liquidator);
+
+        assertTrue(permitted);
+        assertEq(tokenA, 100 * 1e18);
+        assertEq(tokenB, 50 * 1e18);
+    }
+    function testRevertInvalidLiquidity() public {
+        vm.prank(liquidator);
+        mockTokenA.approve(address(DEX), 100 * 1e18);
+        vm.prank(liquidator);
+         mockTokenB.approve(address(DEX), 50 * 1e18);
+        vm.prank(liquidator);
+        vm.expectRevert(DEXLite.invalidLiquidity.selector);
+        DEX.addLiquidity(0 , 50 * 1e18); 
+    }
 }
